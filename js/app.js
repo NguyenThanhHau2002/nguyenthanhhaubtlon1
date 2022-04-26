eb53f2c527c1257bf627a76a63c35f3b5ab3f96d
NguyenThanhHau2002 <99946413+NguyenThanhHau2002@users.noreply.github.com>
2022-04-26 16:24:48 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function name(params) {
+    $("#btnDK").click(function(){
+        $("#myModal").modal();
+    })
+})
+$(document).ready(function name(params) {
+    $("#btnDN").click(function(){
+        $("#myModal1").modal();
+    })
+})
